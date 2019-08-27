db12ac0ee66762427d7beb4084945f7793e2efaa
jeremyduplessis <jeremy.douglas.duplessis@gmail.com>
2019-08-27 18:21:36 +0200

    Test Cases & Main Update

--- Back-end/Main.py
+++ Back-end/Main.py
@@ -5,16 +5,50 @@
 
 from enum import Enum
 
-class Main:
-    '''Driver class for the ViKER Back-end implementation.'''
-    def convertToARM(self):
-        # Produce a JSON representation of given schema in ARM
-    
-    def convertToER(self):
-        # Produce JSON representation of given schema in ER
 
-    def produceOOP(self, file):
-        # Produce OOP representation of schema from given ARM or ER JSON file
+'''Driver class for the ViKER Back-end implementation.'''
+def EERToARM(filePath):
+    # Produce a JSON representation of given schema in ARM
+    entities = fetchEntities(filePath) # read in array of entities from JSON file
+    strongEntities = [E for E in entities if E.isStrong]
+    weakEntities = [E for E in entities if not E.isStrong]
+
+    # For each strong entity E, create a Relation T 
+    Relations = StrongEntityToRelation(strongEntities)
+
+def ARMToEER(self):
+    # Produce JSON representation of given schema in ER
+
+def produceOOP(self, file):
+    # Produce OOP representation of schema from given ARM or ER JSON file
+
+def StrongEntityToRelation(E):
+    # Create the relation
+    T = Relation(
+                name=E.name,
+                inheritsFrom="none", # TO BE UPDATED
+                coveredBy=[], 
+                disjointWith=[]
+                )
+
+    # Add the "self" reference 
+    T.addAttribute(
+                   name="self", 
+                   isConcrete=False, 
+                   dataType=DataTypes.ANY_TYPE, 
+                   isPFD=A.isIdentifier, 
+                   isFK=False
+                   )
+
+    # Add all non-composite (simple) attributes
+    for A in [x for x in E.attributes if len(x.composedOf) == 0]:
+        T.addAttribute(
+                       name=A.name, 
+                       isConcrete=True, 
+                       dataType=DataTypes.ANY_TYPE, 
+                       isPFD=A.isIdentifier, 
+                       isFK=False
+                       )
 
 class DataTypes(Enum):
     '''Specific types of data available'''
