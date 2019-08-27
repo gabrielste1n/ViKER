ca829223e65aeb5b1029230067ac0e2692fa3e68
St John Grimbly <stjohngrimbly@gmail.com>
2019-08-27 20:04:09 +0200

    Update and add accessors and mutators for classes.

--- Back-end/Relationship.py
+++ Back-end/Relationship.py
@@ -7,3 +7,19 @@ class Relationship:
         self.relationshipTypeLocal = relationshipTypeLocal
         self.relationshipTypeForeign = relationshipTypeForeign
         self.attributes = attributes
+
+    def getEntityName(self):
+        '''Returns the name of the entity it is related to'''
+        return self.entityName
+
+    def getLocalRelationship(self):
+        '''Returns the local relationship'''
+        return self.getLocalRelationship
+
+    def getForeignRelationship(self):
+        '''Returns the local relationship'''
+        return self.getForeignRelationship
+
+    def getAttributes(self):
+        '''Returns the relationship attributes'''
+        return self.attributes
