c1c98af3e6228541228a480241a3d3ce9dfe8b09
St John Grimbly <stjohngrimbly@gmail.com>
2019-09-05 18:20:35 +0200

    Document WebServer code. Add diagrams for report.

--- Back-end/WebServer.py
+++ Back-end/WebServer.py
@@ -13,34 +13,52 @@ from flask_cors import CORS
 WebServer = Flask(__name__)
 cors = CORS(WebServer)
 
-def transform(json_file, fileType):
+def transform(json_file, file_type):
+    """
+    Given a json_file and the type of file (ARM/EER), contact the back-end classes and transform the file.
+    Return the transformed file
+
+    Parameters
+    ----------
+    json_file: The JSON ARM/EER file
+
+    file_type: The type of the JSON file. i.e. ARM or EER
+    Returns
+    -------
+    ARMToEER(json_file) or EERToARM(json_file): Transformed file
+    """
+
     print ("At transform method")
     if (fileType == "ARM"):
-        # # --------------------------------------------
-        # filename = '../Documentation/Phase 4/Test Cases/ERtoARM/ER/TestCase1_EER.JSON'
-        # with open(filename, 'r') as json_file: 
-        #     entities = json.load(json_file)
-        # return entities
-        # # --------------------------------------------
         return ARMToEER(json_file)
-    print (json_file)
     return EERToARM(json_file)
 
 def determine_model_type(json_file):
-    # with open(file, 'r') as json_file:
-    #         data = json.load(json_file)
+    """
+    Given a json_file, determine the type of the JSON file representation.
+
+    Parameters
+    ----------
+    json_file: The JSON ARM/EER file
+
+    Returns
+    -------
+    String 'EER' or 'ARM'
+    """
     if 'entities' in json_file: 
         return 'EER'
     return 'ARM'
 
 @WebServer.route('/')
 def index():
+    '''Index page for the server root directory.'''
     # TO DO: Display Markdown user guide for this application
     return ("WELCOME. THIS IS A SERVER ENDPOINT FOR CONVERTING BETWEEN " 
             "ARM AND EER MODELS.")
 
 @WebServer.route('/api/transform', methods=['GET','POST'])
 def transform_view():
+    '''API endpoint for the transform method used in the JavaScript front-end.'''
     json_file = request.get_json(force=True)# JSON Dictionary
     
     #json_file = request.json
